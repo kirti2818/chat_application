19b3d1963b16216dcee424004a67361aa33b7804
kirti2818 <kirti8527260810@gmail.com>
2024-03-11 17:55:32 +0530

    changes

--- frontend/app/api/index.js
+++ frontend/app/api/index.js
@@ -1,10 +1,10 @@
 "use client";
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080/api/";
-// export const baseURL = "https://chat-application-gi09.onrender.com/api/"
-export const clientUrl = "http://localhost:3000/";
-// export const clientUrl = "https://chat-application-ruddy-five.vercel.app/"
+// export const baseURL = "http://localhost:8080/api/";
+export const baseURL = "https://chat-application-gi09.onrender.com/api/"
+// export const clientUrl = "http://localhost:3000/";
+export const clientUrl = "https://chat-application-ruddy-five.vercel.app/"
 import { getCookie } from "cookies-next";
 
 let token;
